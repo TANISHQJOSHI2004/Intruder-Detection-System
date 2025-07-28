5a6a0d3875dc6fd389b966a73dda269cbeb13fcf
Tanishq Prakash Joshi <107499343+TANISHQJOSHI2004@users.noreply.github.com>
2025-07-28 16:25:12 +0530

    Delete static directory

--- static/js/script.js
+++
@@ -1,6 +0,0 @@
-const registerBtn = document.querySelector('.register-btn');
-const registerExtra = document.getElementById('registerExtra');
-
-registerBtn.addEventListener('click', () => {
-    registerExtra.classList.toggle('show');
-});
